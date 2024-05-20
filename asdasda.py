c7743f8dc6201cb89629e1d6fa32a221ce5e22d7
Ildar <kitnekot@icloud.com>
2024-05-20 22:13:21 +0500

    fdfd

--- asdasda.py
+++ asdasda.py
@@ -6,11 +6,16 @@ ewqeqwewq
 <<<<<<< HEAD
 >>>>>>> 4339524 (fefdsfds)
 dwqdwq
+<<<<<<< HEAD
 цццd
 Dwaddwqdwqdwqdwqdwq
 1122
 dwqdwqeqwqw
 12321312
+=======
+PLPLllsl
+dwqdwqdу
+>>>>>>> e8d72ef (dfdsf)
 Hoelo reddwedw
 
 
